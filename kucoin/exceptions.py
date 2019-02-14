0d033440446565311513c5facb6e045fc5e4a3e9
ly <ly.luo@kucoin.com>
2019-02-14 17:47:10 +0800

    add description

--- kucoin/exceptions.py
+++ kucoin/exceptions.py
@@ -1,10 +1,23 @@
 # coding=utf-8
-
-import json
-
+# HTTP error status codes
+# Code	Meaning
+# 400	Bad Request -- Invalid request format
+# 401	Unauthorized -- Invalid API Key
+# 403	Forbidden -- The requested is hidden for administrators only.
+# 404	Not Found -- The specified resource could not be found.
+# 405	Method Not Allowed -- You tried to access the resource with an invalid method.
+# 429	Too Many Requests -- Exceeded the access frequency
+# 500	Internal Server Error -- We had a problem with our server. Try again later.
+# 503	Service Unavailable -- We're temporarily offline for maintenance. Please try again later.
 # System error codes
 # Code	Meaning
-# 403000	No Auth -- The requested is forbidden for current API key.
+# 400001	Any of KC-API-KEY, KC-API-SIGN, KC-API-TIMESTAMP, KC-API-PASSPHRASE is missing in your request header
+# 400002	KC-API-TIMESTAMP Invalid -- Time differs from server time by more than 5 seconds
+# 400003	KC-API-KEY not exists
+# 400004	KC-API-PASSPHRASE error
+# 400005	Signature error -- Please check your signature
+# 400006	The requested ip address is not in the api whitelist
+# 400007	Access Denied -- Your api key does not have sufficient permissions to access the uri
 # 404000	Url Not Found -- The request resource could not be found
 # 400100	Parameter Error -- You tried to access the resource with invalid parameters
 # 411100	User are frozen -- User are frozen, please contact us via support center.
@@ -19,6 +32,7 @@ class KucoinAPIException(Exception):
         `message` format
 
     """
+
     def __init__(self, response):
         self.code = ''
         self.message = 'Unknown Error'
