fe0a299f4d7cf3205ba1c02d4fe14dd90b3b13a6
ly <ly.luo@kucoin.com>
2019-02-22 12:02:52 +0800

    modify version from 2.0.4 to 2.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='kucoin-python',
-    version='2.0.4',
+    version='2.0.5',
     packages=['kucoin'],
     description='Kucoin REST API v2 python implementation',
     url='https://github.com/Kucoin/python-kucoin',
